73000af87870087365b899e07188abe5fa2f2ecd
ppedziwiatr <peter@redstone.finance>
2022-02-21 09:27:52 +0100

    y u no workin'???

--- assembly/handle.ts
+++ assembly/handle.ts
@@ -1,66 +1,10 @@
-// commons imports - do not remove!
-import {
-  Block,
-  console,
-  Contract,
-  ContractFn,
-  HandlerResultSchema,
-  msg,
-  SmartweaveSchema,
-  parse,
-  stringify,
-  Transaction
-} from "./imports";
 
-// contract specific imports
-import {increment} from "./actions/increment";
-import {decrement} from "./actions/decrement";
-import {fullName} from "./actions/fullName";
-import {foreignRead} from "./actions/foreignRead";
-import {infLoop} from "./actions/infLoop";
 
-import {ActionSchema, ResultSchema, StateSchema} from "./schemas";
 
-const functions: Map<string, ContractFn<StateSchema, ActionSchema, ResultSchema>> = new Map();
-functions.set("increment", increment);
-functions.set("decrement", decrement);
-functions.set("fullName", fullName);
-functions.set("foreignRead", foreignRead);
-functions.set("infLoop", infLoop);
+export function add_numbers(arg: i32): void {
+  let first_number;
+  first_number = arg;
 
-export type ContractResultSchema = HandlerResultSchema<StateSchema, ResultSchema>;
-
-let contractState: StateSchema;
-
-@contract
-function handle(action: ActionSchema): ResultSchema | null {
-  console.log(`Function called: "${action.function}"`);
-  console.logO(`Smartweave:`, stringify<SmartweaveSchema>({
-    contract: {
-      id: Contract.id(),
-      owner: Contract.owner()
-    },
-    sender: msg.sender(),
-    block: {
-      height: Block.height(),
-      indep_hash: Block.indep_hash(),
-      timestamp: Block.timestamp()
-    },
-    transaction: {
-      id: Transaction.id(),
-      owner: Transaction.owner(),
-      target: Transaction.target()
-    }
-  }));
-
-  const fn = action.function;
-  if (functions.has(fn)) {
-    const handlerResult = functions.get(fn)(contractState, action);
-    if (handlerResult.state != null) {
-      contractState = handlerResult.state;
-    }
-    return handlerResult.result;
-  } else {
-    throw new Error(`[CE:WTF] Unknown function ${action.function}`);
-  }
+  let second_number = get_second_number();
+  return first_number + second_number;
 }
--- index_handle.js
+++ index_handle.js
@@ -2,14 +2,10 @@ const fs = require("fs");
 const loader = require("@assemblyscript/loader");
 const metering = require('wasm-metering');
 const {Benchmark} = require("redstone-smartweave");
+const Asyncify = require("asyncify-wasm");
 
 const wasmBinary = fs.readFileSync(__dirname + "/build/optimized.wasm");
-const meteredWasmBinary = metering.meterWASM(wasmBinary, {
-  meterType: "i32",
-});
-const wasm2json = require('wasm-json-toolkit').wasm2json
-const json = wasm2json(meteredWasmBinary);
-fs.writeFileSync("wasm_module.json", JSON.stringify(json, null, 2))
+
 
 let limit = 5100000000;
 let gasUsed = 0;
@@ -90,6 +86,12 @@ const imports = {
       console.error(`    on line ${line}, column ${column}.`);
       console.error("------------------------------------------------------------------------------\n");
     },
+
+    async get_answer() {
+      let response = await fetch("answer.txt");
+      let text = await response.text();
+      return Number(text);
+    }
   }
 }
 
@@ -97,116 +99,15 @@ function getFn(idx) {
   return wasmExports.table.get(idx);
 }
 
-const wasmModule = loader.instantiateSync(
-  meteredWasmBinary,
-  imports
-);
+async function main() {
+  const wasmModule = new WebAssembly.Module(wasmBinary);
+  await wasmModule.exports.handle()
 
-const wasmExports = wasmModule.exports;
+  /*const wasmModule = loader.instantiateSync(
+    instance,
+  );*/
 
-const {handle, lang, initState, currentState} = wasmModule.exports;
-const {__newString, __getString, __collect} = wasmModule.exports;
 
-function safeHandle(action) {
-  try {
-    doHandle(action)
-  } catch (e) {
-    // note: as exceptions handling in WASM is currently somewhat non-existent
-    // https://www.assemblyscript.org/status.html#exceptions
-    // and since we have to somehow differentiate different types of exceptions
-    // - each exception message has to have a proper prefix added.
-
-    // exceptions with prefix [RE:] ("Runtime Exceptions") should break the execution immediately
-    // - eg: [RE:OOG] - [RuntimeException: OutOfGas]
-
-    // exception with prefix [CE:] ("Contract Exceptions") should be logged, but should not break
-    // the state evaluation - as they are considered as contracts' business exception (eg. validation errors)
-    // - eg: [CE:WTF] - [ContractException: WhatTheFunction] ;-)
-    if (e.message.startsWith('[RE:')) {
-      throw e;
-    } else {
-      console.error(e.message);
-    }
-  }
 }
 
-function doHandle(action) {
-  // TODO: consider NOT using @assemblyscript/loader and handle conversions manually
-  // - as @assemblyscript/loader adds loads of crap to the output binary.
-  const actionPtr = __newString(JSON.stringify(action));
-  const resultPtr = handle(actionPtr);
-  const result = __getString(resultPtr);
-
-  return JSON.parse(result);
-}
-
-function doInitState() {
-  let statePtr = __newString(JSON.stringify({
-      firstName: 'first_ppe',
-      lastName: 'last_ppe',
-      counter: 0
-    }
-  ));
-
-  initState(statePtr);
-
-  gasUsed = 0;
-}
-
-function doGetCurrentState() {
-  const currentStatePtr = currentState();
-  return JSON.parse(wasmExports.__getString(currentStatePtr));
-}
-
-
-const actions = [
-  {function: 'foreignRead', contractTxId: 'sdfsdf23423sdfsdfsdfsdfsdfsdfsdfsdf'}
-  /*{function: 'increment'},
-  {function: 'decrement'},
-  {function: 'increment'},
-  {function: 'fullName'},
-  {function: 'unknownFn'}, /!* this one should throw unknown function *!/
-  {function: 'increment'}, /!* this one should throw out of gas *!/
-  {function: 'decrement'}  /!* this one should not be called *!/*/
-]
-
-// note: this will be useful in SDK to prepare the wasm execution env. properly
-// for contracts written in different langs (eg. in assemblyscript we can use the
-// built-in @assemblyscript/loader to simplify the communication - but obv. it wont' be available
-// in Rust or Go)
-console.log("Contract language:", __getString(lang));
-
-//(o) initialize the state in the wasm contract
-doInitState();
-
-//(o) evaluate all actions
-for (const action of actions) {
-  console.log("==============================================================================")
-  const handlerResult = safeHandle(action);
-  console.log({
-    handlerResult,
-    state: doGetCurrentState(),
-    gas: `${formatGas(gasUsed)}`,
-    gasLimit: `${formatGas(limit)}`
-  });
-}
-
-// (o) re-init the state
-/*doInitState();
-console.log("Current state", doGetCurrentState());
-limit = limit * 100000;
-
-const benchmark = Benchmark.measure();
-for (let i = 0; i < 1_000_000; i++) {
-  if (i % 100_000 == 0) {
-    console.log('calling', i + 1);
-  }
-  safeHandle({function: 'increment'});
-}
-console.log("Computed 1M interactions in", benchmark.elapsed());
-console.log("Current state", doGetCurrentState());
-console.log("Gas used", formatGas(gasUsed));*/
-
-function formatGas(gas) {
-  return gas * 1e-4;
-}
+main().finally();

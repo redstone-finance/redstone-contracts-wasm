c5cf7c979df7a4189a5140cb71fe50756a380627
ppedziwiatr <peter@redstone.finance>
2022-03-12 20:40:13 +0100

    feat: state copy for as contracts

--- assemblyscript/assembly/imports/api.ts
+++ assemblyscript/assembly/imports/api.ts
@@ -1,30 +1,13 @@
-export declare function _setTimeout(fn: usize, milliseconds: f32): i32
-
-export function setTimeout<T = unknown>(fn: (t: T) => void, milliseconds: f32 = 0.0): i32 {
-  return _setTimeout(fn.index, milliseconds)
-}
-
-export declare function clearTimeout(id: i32): void
-
-
-export declare function _readContractState(fn: usize, contractTxId: string): i32;
-
-// note: this requires adding --exportTable to asc
-export function readContractState(fn: (t: string) => void, contractTxId: string): i32 {
-  return _readContractState(fn.index, contractTxId);
-}
-
 export type ContractFn<S, A, R> = (state: S, action: A) => HandlerResultSchema<S, R>;
-
 
 @serializable
 export class HandlerResultSchema<S, R> {
-  state: S
+  state: S | null
   result: R | null
 }
 
 @serializable
-export class SmartweaveSchema {
+export class SmartWeaveSchema {
   contract: ContractSchema
   block: BlockSchema
   transaction: TransactionSchema

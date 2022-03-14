6ef9cd441241f020a96f9f6351eeb105b49f2a60
hatskier <suvorovalex1997@gmail.com>
2022-03-14 01:02:32 +0100

    added a comment about error handling in foreign calls

--- rust/src/contract_utils/foreign_call.rs
+++ rust/src/contract_utils/foreign_call.rs
@@ -5,7 +5,7 @@ pub async fn read_foreign_contract_state<T: DeserializeOwned>(contract_address: 
   let state: T = SmartWeave::read_contract_state(contract_address)
     .await
     .into_serde()
-    .unwrap();
+    .unwrap(); // TODO: not sure if it won't case panics. Maybe it's better to return Result<T, ContractError>
 
   return state;
 }
